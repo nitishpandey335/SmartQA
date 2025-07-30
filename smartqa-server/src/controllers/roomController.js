7a7772c61321a422c43123a406a8406be51d24ee
Nitish Kumar Pandey <117139675+nitishpandey335@users.noreply.github.com>
2025-07-30 20:23:30 +0530

    Add files via upload

---
+++ smartqa-server/src/controllers/roomController.js
@@ -0,0 +1,93 @@
+const Rooms = require("../models/Rooms");
+const Questions = require("../models/Questions");
+const { callGemini } = require("../services/geminiService");
+
+const roomController = {
+    createRoom: async (request, response) => {
+        try {
+            const { createdBy } = request.body;
+
+            const code = Math.random().toString(36)
+                .substring(2, 8).toUpperCase();
+
+            const room = await Rooms.create({
+                roomCode: code,
+                createdBy: createdBy
+            });
+
+            response.json(room);
+        } catch (error) {
+            console.log(error);
+            response.status(500).json({ message: 'Internal server error' });
+        }
+    },
+
+    getByRoomId: async (request, response) => {
+        try {
+            const code = request.params.code;
+
+            const room = await Rooms.findOne({ roomCode: code });
+            if (!room) {
+                return response.status(404).json({ message: 'Room not found' });
+            }
+
+            response.json(room);
+        } catch (error) {
+            console.log(error);
+            response.status(500).json({ message: 'Internal server error' });
+        }
+    },
+
+    createQuestion: async (request, response) => {
+        try {
+            const { content, user } = request.body;
+            const roomCode = request.params.code;
+
+            const question = await Questions.create({
+                roomCode: roomCode,
+                content: content,
+                user: user
+            });
+
+            const io = request.app.get("io");
+            io.to(roomCode).emit("new-question", question);
+
+            response.json(question);
+        } catch (error) {
+            console.log(error);
+            response.status(500).json({ message: 'Internal server error' });
+        }
+    },
+
+    getQuestions: async (request, response) => {
+        try {
+            const code = request.params.code;
+
+            const questions = await Questions
+                .find({ roomCode: code })
+                .sort({ createdAt: -1 });
+            
+            response.json(questions);
+        } catch (error) {
+            console.log(error);
+            response.status(500).json({ message: 'Internal server error' });
+        }
+    },
+
+    summarizeQuestions: async (request, response) => {
+        try {
+            const { code } = request.params;
+
+            const questions = await Questions.find({ roomCode: code });
+            if (questions.length === 0) return response.json([]);
+
+            const summary = await callGemini(questions);
+            response.json(summary);
+        } catch (error) {
+            console.log(error);
+            response.status(500).json({ message: 'Internal server error' });
+        }
+    },
+};
+
+module.exports = roomController;
